db02a30b53493a8f262f6e0536665409d7c309b2
kugiyasan <leobannocloutier@gmail.com>
2020-07-07 21:25:46 -0400

    trying to deploy to heroku

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,6 +1,21 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, menus
 
+class MyMenu(menus.Menu):
+    async def send_initial_message(self, ctx, channel):
+        return await channel.send(f'Hello {ctx.author}')
+
+    @menus.button('\N{THUMBS UP SIGN}')
+    async def on_thumbs_up(self, payload):
+        await self.message.edit(content=f'Thanks {self.ctx.author}!')
+
+    @menus.button('\N{THUMBS DOWN SIGN}')
+    async def on_thumbs_down(self, payload):
+        await self.message.edit(content=f"That's not nice {self.ctx.author}...")
+
+    @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
+    async def on_stop(self, payload):
+        self.stop()
 
 class Dev(commands.Cog):
     def __init__(self, bot):
@@ -17,6 +32,11 @@ class Dev(commands.Cog):
         embed = discord.Embed()
         embed.set_image(url="attachment://image.png")
         await ctx.send(file=file, embed=embed)
+
+    @commands.command()
+    async def menu(self, ctx):
+        m = MyMenu(timeout=60.0, clear_reactions_after=True)
+        await m.start(ctx)
 
 
 def setup(bot):
