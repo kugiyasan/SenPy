2771e25a675a42e284c61eca86b8a8c84ba1334a
kugiyasan <leobannocloutier@gmail.com>
2020-07-21 18:14:07 -0400

    change sqlite query to postgresql query

--- cogs/utils/dbms.py
+++ cogs/utils/dbms.py
@@ -1,5 +1,13 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+try:
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+except:
+    from dotenv import load_dotenv
+    load_dotenv()
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL)
+
+cursor = conn.cursor()
