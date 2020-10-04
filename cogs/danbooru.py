7eeff8b97455f8ff54f3f51ed2cc3afa7207a162
kugiyasan <leobannocloutier@gmail.com>
2020-10-04 18:14:01 -0400

    added shortcuts for danbooru

--- cogs/danbooru.py
+++ cogs/danbooru.py
@@ -5,6 +5,7 @@ from pybooru import Danbooru
 import random
 import requests
 from cogs.utils.sendEmbed import sendEmbed
+from cogs.mofupoints import incrementEmbedCounter
 
 
 class DanbooruCog(commands.Cog, name="Danbooru"):
@@ -12,37 +13,124 @@ class DanbooruCog(commands.Cog, name="Danbooru"):
         self.bot = bot
         self.urlsTags = {}
         self.client = Danbooru("danbooru")
+        self.clovisProtection = True
+        self.shortcuts = {
+            "unicorn": "unicorn_(azur_lane)",
+            "rem": "rem_(re:zero)",
+            "emilia": "emilia_(re:zero)",
+            "hanamaru": "kunikida_hanamaru",
+            "maru": "kunikida_hanamaru",
+            "zero_two": "zero_two_(darling_in_the_franxx)",
+            "02": "zero_two_(darling_in_the_franxx)",
+            "megumin": "megumin",
+            "ruka": "sarashina_ruka",
+            "sumi": "sakurasawa_sumi",
+            "chika": "fujiwara_chika",
+            "shiro": "shiro_(sewayaki_kitsune_no_senko-san)",
+            "you": "watanabe_you",
+            "ruby": "kurosawa_ruby",
+            "korone": "inugami_korone",
+        }
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # Check if the message starts with the command prefix
-        pass
+        if message.author.bot:
+            return
+
+        ctx = await self.bot.get_context(message)
+        if ctx.command is None and ctx.prefix is not None:
+            key = ctx.message.content[len(ctx.prefix):]
+            tags = self.shortcuts.get(key, None)
+
+            if tags:
+                try:
+                    if not ctx.channel.is_nsfw():
+                        raise commands.errors.NSFWChannelRequired(ctx.channel)
+
+                    await self.danbooru(ctx, tags=tags)
+                except commands.errors.NSFWChannelRequired as err:
+                    #! should handle the errors with the default on_command_error
+                    await ctx.send(err)
+                    return
 
     @commands.command()
-    async def searchdanbooru(self, ctx, searchWord):
-        """Finally a danbooru search command"""
-        pass
+    async def waifus(self, ctx):
+        longestKey = max(len(k) for k in self.shortcuts.keys())
+        shortcuts = "**Available shortcuts:**```"
+        for k, v in self.shortcuts.items():
+            shortcut = f"\n{k}:" + ' ' * (longestKey - len(k) + 1) + v
+            if len(shortcuts) + len(shortcut) > 2000-3:
+                await ctx.send(shortcuts + "```")
+                shortcuts = "```" + shortcut
+            else:
+                shortcuts += shortcut
 
+        await ctx.send(f"{shortcuts}```You can add your waifus using xd addshortcut :)")
+
+    @commands.cooldown(5, 3600.0, commands.BucketType.user)
+    @commands.command()
+    async def addshortcut(self, ctx, key, danbooruTags):
+        """add a shortcut for your waifu. 1 hour cooldown, so use it carefully"""
+        if self.shortcuts.get(key, None):
+            await ctx.send("There is already a shortcut with that key!")
+        elif self.bot.get_command(key):
+            await ctx.send("You can't name a shortcut with the name of a command!")
+        elif len(key) > 20:
+            await ctx.send("The key is too long, make it shorter")
+        else:
+            self.shortcuts[key] = danbooruTags
+            await ctx.message.add_reaction("✅")
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def delshortcut(self, ctx, key):
+        if not self.shortcuts.get(key, None):
+            await ctx.send("The shortcut doesn't exist!")
+            return
+
+        del self.shortcuts[key]
+        await ctx.message.add_reaction("✅")
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def togglensfw(self, ctx):
+        self.clovisProtection = not self.clovisProtection
+        if self.clovisProtection:
+            await ctx.message.add_reaction("✅")
+        else:
+            await ctx.message.add_reaction("❎")
+
+    @commands.is_nsfw()
     @commands.command(aliases=["db", "dan"])
     async def danbooru(self, ctx: commands.Context, *, tags="rating:s"):
         """Wow searching on danbooru that's so original"""
-        # f"search[name_matches]={tags}*"
-
         if not len(self.urlsTags.get(tags, {})):
             await self.requestDanbooru(tags)
+
+            if not len(self.urlsTags.get(tags, {})):
+                await ctx.send("I couldn't find any image with that tag!")
+                return
 
         post = self.urlsTags[tags].pop()
         if post["rating"] != "s" and not ctx.channel.is_nsfw():
             raise commands.errors.NSFWChannelRequired(ctx.channel)
 
+        if ctx.author.id == 276038064273489930 and self.clovisProtection == True:
+            if "rating:e" in tags:
+                await ctx.send("I couldn't find any image with that tag!")
+                return
+
+            while post["rating"] == "e":
+                print(f"dropping {post}")
+                post = self.urlsTags[tags].pop()
+
         await ctx.send(embed=self.danbooruEmbed(post))
+        await incrementEmbedCounter(ctx.author)
 
     async def requestDanbooru(self, tags):
         print("resquesting danbooru")
 
-        randompage = random.randint(1, 100)
-        posts = self.client.post_list(
-            tags=tags, page=randompage, limit=200, random=True)
+        posts = self.client.post_list(tags=tags, limit=100, random=True)
 
         if len(posts) < 1:
             print("No posts with those tags")
@@ -64,11 +152,12 @@ class DanbooruCog(commands.Cog, name="Danbooru"):
         print(f"{len(self.urlsTags[tags])} urls for {tags}")
 
     def danbooruEmbed(self, post):
-        print(post["file_url"])
+        post_url = f"https://danbooru.donmai.us/posts/{post['id']}"
+        print(post_url)
         embed = discord.Embed(
             color=discord.Colour.gold(),
-            title=f"source",
-            url=f"https://danbooru.donmai.us/posts/{post['id']}"
+            title=f"sauce",
+            url=post_url
         )
 
         embed.set_image(url=post["file_url"])
